5ef8ab55e6f1ef8a52fac5c907b661844d616b30
Alex Tomlinson <1alextomlinson@gmail.com>
2020-09-24 12:16:42 -0400

    ~

---
+++ script.js
@@ -0,0 +1,65 @@
+$( ".toggledark" ).click(function() {
+  $("body").toggleClass("dark");
+
+if ($("body").hasClass("dark")) {
+
+    $(".toggledark").html("ó");
+    document.documentElement.style.setProperty('--type', 'white');
+  document.documentElement.style.setProperty('--bg', 'black');
+
+} else {
+
+  $(".toggledark").html("3");
+
+  document.documentElement.style.setProperty('--type', 'black');
+  document.documentElement.style.setProperty('--bg', 'white');
+}
+
+});
+
+function darkmode() {
+
+document.documentElement.style.setProperty('--type', randomColor());
+document.documentElement.style.setProperty('--bg', 'black');
+
+}
+
+function lightmode() {
+
+document.documentElement.style.setProperty('--type', randomColor({luminosity: 'dark'}));
+document.documentElement.style.setProperty('--bg', randomColor({luminosity: 'light'}));
+
+}
+
+
+
+$( ".togglerandom" ).click(function() {
+
+  if (Math.random()>.5) { darkmode()} else
+
+  {lightmode()}
+
+});
+
+$( ".toggleabout" ).click(function() {
+
+  $(".about").toggleClass("aboutshow")
+
+});
+
+$( ".x" ).click(function() {
+
+  $(".about").removeClass("aboutshow")
+
+});
+
+
+$( ".grinnell" ).keyup(function() {
+  if ($(this).html() == "BIRD LORE") {
+
+    $(".download").addClass("dlshow")
+
+  }
+
+
+});
